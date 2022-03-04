7f02e287f992e074f8a1923204ada1c00a7f0a03
TomasWard1 <35110844+TomasWard1@users.noreply.github.com>
2022-03-04 12:15:45 -0300

    Add files via upload

---
+++ learning.py
@@ -0,0 +1,26 @@
+#tipos de datos en pyhton
+numeroEntero = int(5)
+numeroEntero2 = int(-5) #numeros enteros (Z) pero son finitos por el limite de procesamiento de una computadora.
+numeroEntero3 = int(0)
+
+#operaciones
+def valorAbsoluto(numero):
+    abs(numero)
+
+
+
+def estamosEnClase(enClase:bool):
+    if enClase== True:
+        return'Estamos en clase'
+    else:
+        return 'No estamos en clase'
+
+#PROMEDIO = TP1 * 0,2 + P1 * 0,2 + TP2 * 0,3 + P2 * 0,3
+#debe ser mayor o igual a 60 para promocionar.
+
+def promedioPonderado(TP1:int,P1:int,TP2:int,P2:int) -> int:
+    return TP1 * 0.2 + TP1 * 0.2 + TP1 * 0.2 + TP1 * 0.2
+
+
+#RunTIME
+print(estamosEnClase(False))
