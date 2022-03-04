c7eee4daa848f4ac3f0f21c28e3fca92e24e2b69
TomasWard1 <tomas@familiaward.com.ar>
2022-03-04 17:12:20 -0300

    third commit

--- learning.py
+++ learning.py
@@ -5,20 +5,21 @@ numeroEntero3 = int(0)
 
 numeroFloat = float(5.3)
 
-frase = string('hola mundo')
+frase = str('hola mundo')
 
-#operaciones
+#operaciones-
 def valorAbsoluto(numero):
     abs(numero)
 
 #funciones
-def estamosEnClase(enClase:bool):
+def estamosEnClase(enClase):
     if enClase== True:
         return'Estamos en clase'
     else:
         return 'No estamos en clase'
 
-def promedioPonderado(TP1:int,P1:int,TP2:int,P2:int) -> int:
-    return TP1 * 0.2 + TP1 * 0.2 + TP1 * 0.2 + TP1 * 0.2
+def promedioPonderado(TP1,P1,TP2,P2):
+     print(TP1 * 0.2 + TP1 * 0.2 + TP1 * 0.2 + TP1 * 0.2)
 
 #RunTIME
+promedioPonderado(5,4,10,3)
